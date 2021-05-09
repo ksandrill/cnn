bcf441de0a5b92d1b46271e3d173eb45bbee9114
ksandrill <edgyestlord@gmail.com>
2021-05-09 14:38:22 +0700

    init

---
+++ mnist_cnn/cnn/__init__.py
@@ -0,0 +1,5 @@
+from cnn.train import train
+from cnn.utils import Params, load_mnist, CLASS_NUMBER
+from cnn.feed_forward import classify
+
+__all__ = ['train', 'Params', 'classify', 'load_mnist', 'CLASS_NUMBER']
